384e362440beed0fefed4a2d9d3d575d154159ce
Administrator <769783046@qq.com>
2026-09-02 00:41:32 +0800

    update vite and github action

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig({
+  base: "/ReadVerse/",
   plugins: [react(), tailwindcss()],
   clearScreen: false,
   server: {
